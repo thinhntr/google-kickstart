ab6b6c75d7eb148ef88fdeab240b87f779f32f39
thinhntr <thinhntr@gmail.com>
2021-02-04 18:15:17 +0700

    Update solution

--- 2020/c/countdown/v2.py
+++ 2020/c/countdown/v2.py
@@ -5,12 +5,14 @@ for t in range(1, T + 1):
     count = 0
     k = K
     for num in A:
-        if num == K:
-            k = K - 1
         if num == k:
             if k == 1:
                 count += 1
                 k = K
             else:                
                 k -= 1
+        elif num == K:
+            k = K - 1
+        else:
+            k = K
     print(f"Case #{t}: {count}")
