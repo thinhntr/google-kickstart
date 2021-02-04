5379a49d2e741af5f7b6bd6a6d9843a9d1f6963e
thinhntr <thinhntr@gmail.com>
2021-02-04 22:17:17 +0700

    Update solution

--- 2020/f/atm-queue/v1.py
+++ 2020/f/atm-queue/v1.py
@@ -1,16 +1,18 @@
+from collections import deque
 T = int(input().strip())
 for t in range(1, T + 1):
     N, X = map(int, input().split())
-    A = list(map(int, input().split()))
-    ids = [_ for _ in range(1, N + 1)]
-    leaved_ids = []
-    while len(A) > 0:
+    A = deque(map(int, input().split()))
+    ids = deque([_ for _ in range(1, N + 1)])
+    leaved_ids = deque()
+    while N > 0:
         if A[0] <= X:
-            A.pop(0)
-            leaved_ids.append(ids.pop(0))
+            A.popleft()
+            leaved_ids.append(ids.popleft())
+            N -= 1
         else:
             A[0] -= X
-            A.append(A.pop(0))
-            ids.append(ids.pop(0))
+            A.append(A.popleft())
+            ids.append(ids.popleft())
 
     print(f"Case #{t}: {' '.join(map(str, leaved_ids))}")
