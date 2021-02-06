61d810ea4b261d273b156ec45363448ab6078c63
thinhntr <thinhntr@gmail.com>
2021-02-06 17:46:29 +0700

    Init problem 2 in round c 2020

---
+++ 2020/c/stable-wall/v1.py
@@ -0,0 +1,24 @@
+def print_result(order, result):
+    print(f"Case #{order + 1}: {result}")
+
+
+def readln():
+    return input().strip()
+
+
+def read_int():
+    return int(input().strip())
+
+
+def read_ints():
+    return list(map(int, input().split()))
+
+
+def solve(order):
+    print_result(order, "#TODO")
+
+
+if __name__ == "__main__":
+    T = read_int()
+    for t in range(T):
+        solve(t)
